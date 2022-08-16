0b0e85e63d6cd42202671e56d0cc91ef912f9893
Paul Owe <paul.owe@live.com>
2022-08-15 21:19:51 -0400

    Update deploy_jupyter.sh

--- deploy_jupyter.sh
+++ deploy_jupyter.sh
@@ -3,52 +3,29 @@
 # Date: Aug 15
 # Purpose: Automatically set up Jupyter server on EC2
 
-
-# Stop if anything fails
-#set -ex
-
-# Get anaconda
-#wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
-
-#echo | bash Anaconda3-2019.03-Linux-x86_64.sh
-
+# Update packages
 sudo apt-get update
 
-#ssh-keygen -t rsa -P ""
-
-#cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-
-#ssh localhost
-
+# Install Python3 and Pip
 sudo apt-get install python3
-
 sudo apt-get -y install python3-pip
 
+#Install Jupyter and Jupyter server
 sudo pip3 install jupyter
-
 sudo pip3 install jupyter_server
 
-#cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-
+#Generate notebook config
 jupyter notebook --generate-config
 
-
-#export PATH=/home/ubuntu/anaconda3/bin:$PATH
-
-#source .bashrc
-
-#jupyter notebook --generate-config
-
+# Set notebook config from prewritten file :)
 git clone https://github.com/paulowe/jupyterserver.git
-
 cd .jupyter
-
 rm jupyter_notebook_config.py
-
 mv ~/jupyterserver/jupyter_notebook_config.py .
 
+# Create folder to store Notebooks
 mkdir MyNotebooks
-
 cd MyNotebooks
 
+#Start Jupyter server
 jupyter notebook
